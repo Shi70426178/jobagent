45d2dbd9a07b7faf62ff00e0fd949d2acab13921
Shi70426178 <159864887+Shi70426178@users.noreply.github.com>
2026-08-12 12:17:12 +0530

    deeee

--- backend/app/api/gmail.py
+++ backend/app/api/gmail.py
@@ -31,6 +31,7 @@ router = APIRouter()
 SCOPES = [
     "openid",
     "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
     "https://www.googleapis.com/auth/gmail.send",
 ]
 
